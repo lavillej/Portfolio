e54662cb3d9355c1b9f04b6221138b4a1797b678
Josh <jclaville93@gmail.com>
2024-06-08 20:38:11 -0400

    Add app description, image, and links

--- send_email.py
+++ send_email.py
@@ -1,20 +1,14 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "jclaville93@gmail.com"
-password = "lzpj nmzz bssg sstc"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "jclaville93@gmail.com"
+    password = "lzpj nmzz bssg sstc"
+    receiver = "jclaville93@gmail.com"
+    context = ssl.create_default_context()
 
-receiver = "thenewlaville@gmail.com"
-context = ssl.create_default_context()
-
-message = """\
-Subject: You have a nice booty!
-Booty Booty Booty
-Rocking everywhere!
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
